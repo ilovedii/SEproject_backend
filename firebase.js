2a88b65c7f2112899efda1b2e21e069a67bc5e1e
ilovedii <gaiwain1310@gmail.com>
2025-04-04 23:14:29 +0800

    User db

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLfYjV9dfKOalCwBISsROQz_zgs9M65Dg",
+  authDomain: "software-project-a060c.firebaseapp.com",
+  projectId: "software-project-a060c",
+  storageBucket: "software-project-a060c.firebasestorage.app",
+  messagingSenderId: "163710286863",
+  appId: "1:163710286863:web:c68c9c05fe96e87d54f77a",
+  measurementId: "G-2F00Q3B6MQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ storeUser.js
@@ -0,0 +1,76 @@
+import { initializeApp, cert } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import {readFileSync} from "fs";
+const serviceAccount = JSON.parse(readFileSync("./accountKey.json", "utf-8"));
+
+//先用terminal try
+import readline from 'readline';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function register(query){
+	return new Promise(resolve => rl.question(query, answer => resolve(answer)));
+}
+
+function info(query){
+	return new Promise(resolve => rl.question(query, answer => resolve(answer)));
+}
+
+
+//啟動firebase SDK
+initializeApp({
+	credential: cert(serviceAccount), //啟動金鑰
+})
+
+const DATABASE = getFirestore();
+const AUTH = getAuth();
+
+function checkEmail(password){
+	const isUpper = /[A-Z]/.test(password);
+	const isLower = /[a-z]/.test(password);
+	const isNumber = /[0-9]/.test(password);
+    const enoughLength = password.length >= 8;
+
+	return isUpper && isLower && isNumber && enoughLength;
+}
+
+async function storeUser(){
+	const email = await register('Email: ');
+	const password = await register('Password: ');
+	
+	if(!checkEmail(password)){
+		console.log("Invalid password");
+		rl.close()
+		return;
+	}
+	
+	try {
+		// create account
+		const user = await AUTH.createUser({email,password});
+
+		const name = await info("Please enter your username: ");
+    	const bio = await info("Please enter your bio: ");
+
+		await DATABASE.collection('users').doc(user.uid).set({
+			username: name,
+			email: user.email,
+			bio: bio,
+			rofile_img: null,
+			url: ''
+		});
+
+		console.log('User successfully added');
+	}
+
+	catch(error){
+		console.error('Invalid User', error.message);
+	}
+	finally{
+		rl.close();
+	}
+}
+
+storeUser()
